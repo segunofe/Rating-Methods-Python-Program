0511a1b435c35d45931d9253ea0a85ca15689cd4
Segun Ofe <83200055+segunofe@users.noreply.github.com>
2026-01-29 14:09:04 -0800

    Update ratingmtds.py

--- ratingmtds.py
+++ ratingmtds.py
@@ -1,4 +1,4 @@
-## Created on Mon Apr 11 18:53:10 2022
+### Created on Mon Apr 11 18:53:10 2022
 
 #@author: Ofe Segun
 
@@ -300,6 +300,7 @@ window.mainloop()
 #https://www.journaldev.com/45109/normalize-data-in-python # helpful to give insight to renormalizing dataset
 #https://www.geeksforgeeks.org/python-tkinter-how-to-display-a-table-editor-in-a-text-widget/?ref=rp #GUI
 #https://www.webucator.com/article/python-color-constants-module/ # Choosing Colors
+
 
 
 
